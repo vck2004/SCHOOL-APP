bb788a8d782a2890795426c66f133255073cdd6d
vck2004 <vckkcv2004@gmail.com>
2024-03-07 18:12:06 +0700

    should work now

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: ['./src/index.js','./src/component.js','./src/model.js','./src/controller.js','./src/view.js'],
     output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
