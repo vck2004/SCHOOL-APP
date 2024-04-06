b7cd83324f8bcc8212afab96ab50d09860e8a466
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2024-04-06 11:11:44 +0700

    Student_profile and edit

---
+++ Khuê/edit_student_profile.js
@@ -0,0 +1,5 @@
+var mainContent = document.getElementById('main_content');
+    document.getElementById('side_bar_button').addEventListener('click',() => {
+    document.getElementById('side_bar').classList.toggle('expand');
+    });
+
